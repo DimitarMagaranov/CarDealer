110f28b58c6c9d47edde8d19b1bd002ee3d4dde3
Dimitar Magaranov <dimitar.magaranov1@gmail.com>
2020-09-12 17:06:05 +0300

    Create CarDealer.sln wit empty projects: CarDealer.ConsoleApplication, CarDealer.Data, CarDealer.Models, CarDealer.Services

---
+++ CarDealer.ConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarDealer.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
