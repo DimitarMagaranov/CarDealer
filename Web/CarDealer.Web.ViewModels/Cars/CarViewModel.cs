b286c0eb86d5a18d0521ce8fa2c165ffa607b019
Dimitar Magaranov <dimitar.magaranov1@gmail.com>
2020-11-10 09:51:41 +0200

    Create AddCarInputModel, AddSaleInputModel, SearchFormInputModel, CarViewModel, SaleViewModel

--- Web/CarDealer.Web.ViewModels/Cars/CarViewModel.cs
+++ Web/CarDealer.Web.ViewModels/Cars/CarViewModel.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CarDealer.Web.ViewModels.Cars
+﻿namespace CarDealer.Web.ViewModels.Cars
 {
-    class CarViewModel
+    using System;
+    using System.Globalization;
+
+    public class CarViewModel
     {
+        public int Id { get; set; }
+
+        public string Make { get; set; }
+
+        public string Category { get; set; }
+
+        public string FuelType { get; set; }
+
+        public int EngineSize { get; set; }
+
+        public string EuroStandart { get; set; }
+
+        public string Gearbox { get; set; }
+
+        public string Color { get; set; }
+
+        public string Doors { get; set; }
+
+        public string State { get; set; }
+
+        public int Mileage { get; set; }
+
+        public DateTime ManufactureDate { get; set; }
+
+        public string ManufactureDateAsString => this.ManufactureDate.ToString(CultureInfo.GetCultureInfo("bg-BG"));
     }
 }
