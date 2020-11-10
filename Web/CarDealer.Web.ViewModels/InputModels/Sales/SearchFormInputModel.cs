fb7129901922b6d94774344eb5a15a2ffd26bf6a
Dimitar Magaranov <dimitar.magaranov1@gmail.com>
2020-11-10 09:47:52 +0200

    Create AddCarInputModel, AddSaleInputModel, SearchFormInputModel, CarViewModel, SaleViewModel

--- Web/CarDealer.Web.ViewModels/InputModels/Sales/SearchFormInputModel.cs
+++ Web/CarDealer.Web.ViewModels/InputModels/Sales/SearchFormInputModel.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CarDealer.Web.ViewModels.InputModels.Sales
+﻿namespace CarDealer.Web.ViewModels.InputModels.Sales
 {
-    class SearchFormInputModel
+    public class SearchFormInputModel
     {
+        public string Category { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string State { get; set; }
+
+        public string ManufactureYearFrom { get; set; }
+
+        public string ManufactureYearTo { get; set; }
+
+        public string PriceFrom { get; set; }
+
+        public string PriceTo { get; set; }
+
+        public string FuelType { get; set; }
+
+        public string Gearbox { get; set; }
+
+        public string EngineSizeFrom { get; set; }
+
+        public string EngineSizeTo { get; set; }
+
+        public string MileageTo { get; set; }
+
+        public string Color { get; set; }
+
+        public string EuroStandart { get; set; }
+
+        public string Region { get; set; }
+
+        public string SortCommand { get; set; }
     }
 }
--- Web/CarDealer.Web.ViewModels/Sales/SaleViewModel.cs
+++ Web/CarDealer.Web.ViewModels/Sales/SaleViewModel.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CarDealer.Web.ViewModels.Sales
+﻿namespace CarDealer.Web.ViewModels.Sales
 {
-    class SaleViewModel
+    using System;
+    using System.Globalization;
+
+    using CarDealer.Web.ViewModels.Cars;
+
+    public class SaleViewModel
     {
+        public int Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string CreatedOnAsString => this.CreatedOn.ToString(CultureInfo.GetCultureInfo("bg-BG"));
+
+        public int CarId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Region { get; set; }
+
+        public string UserFirstName { get; set; }
+
+        public string UserLastName { get; set; }
+
+        public string UserPhoneNumber { get; set; }
+
+        public string Description { get; set; }
+
+        public CarViewModel Car { get; set; }
     }
 }
