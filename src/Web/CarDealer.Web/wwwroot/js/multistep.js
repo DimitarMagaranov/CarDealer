36402623d31956e0c277a57413a441b837d50e96
Dimitar Magaranov <dimitar.magaranov1@gmail.com>
2020-12-08 11:34:31 +0200

    Create multiform for edit sale

--- src/Web/CarDealer.Web/wwwroot/js/multistep.js
+++ src/Web/CarDealer.Web/wwwroot/js/multistep.js
@@ -32,7 +32,7 @@ function nextPrev(n) {
     // if you have reached the end of the form... :
     if (currentTab >= x.length) {
         //...the form gets submitted:
-        document.getElementById("addSaleForm").submit();
+        document.getElementById("multiForm").submit();
         return false;
     }
     // Otherwise, display the correct tab:
